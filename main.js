62c1dc50ca850610f136d9c4914c5805a6ace79e
hyungJinn <hyungjinnh@gmail.com>
2023-02-13 14:00:36 +0900

    Transparent navbar

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Make navbar transparent when it is on the top
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+// https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
